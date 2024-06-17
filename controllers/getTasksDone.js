a5b086231221beff2bc700265ef0831b874b8c83
Vansh Arora <147429499+vansh-9878@users.noreply.github.com>
2024-06-17 17:01:26 +0530

    Create getTasksDone.js

---
+++ controllers/getTasksDone.js
@@ -0,0 +1,73 @@
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+    return;
+  } catch (error) {
+    res.status(500).json({ err_msg: error });
+    return;
+  }
+};
+
+const createTasks = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ msg: "No task with this id" });
+    }
+    res.status(200).json(task);
+    return;
+  } catch (error) {
+    res.status(500).json({ msg: "error" });
+  }
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: "No task with this id" });
+    }
+
+    res.json({ id: taskId, data: req.body });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ msg: "No task with this id" });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+module.exports = {
+  getAllTasks,
+  createTasks,
+  getTask,
+  updateTask,
+  deleteTask,
+};
